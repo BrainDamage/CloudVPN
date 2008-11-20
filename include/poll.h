41c876c62506bab070eba6104c7f5fd052d70841
Mirek Kratochvil <exa.exa@gmail.com>
2008-11-20 20:02:07 +0100

    poll skeleton

---
+++ include/poll.h
@@ -0,0 +1,11 @@
+
+#ifndef _CVPN_POLL_H
+#define _CVPN_POLL_H
+
+int poll_init();
+int poll_deinit();
+int poll_recreate_set();
+int poll_wait_for_event(int timeout);
+
+#endif
+
---
+++ src/poll.cpp
@@ -0,0 +1,25 @@
+
+#include "poll.h"
+
+//TODO decide how to switch between epoll and select.
+
+int poll_init()
+{
+	return 0;
+}
+
+int poll_deinit()
+{
+	return 0;
+}
+
+int poll_recreate_set()
+{
+	return 0;
+}
+
+int poll_wait_for_event(int timeout)
+{
+	return 0;
+}
+
