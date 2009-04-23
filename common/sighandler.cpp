caa7219c78d8e2536d66e46c0fa6b439e7460422
Mirek Kratochvil <exa.exa@gmail.com>
2009-04-23 12:53:01 +0200

    moved sighandler to separate file

---
+++ common/sighandler.cpp
@@ -0,0 +1,30 @@
+
+#include "sighandler.h"
+
+#include "log.h"
+
+
+#include <signal.h>
+
+int setup_sighandler(void(*func)(int))
+{
+#ifndef __WIN32__
+	struct sigaction a;
+
+	Log_info ("setting up signal handler");
+
+	sigemptyset (&a.sa_mask);
+	a.sa_flags = 0;
+	a.sa_handler = func;
+
+	sigaction (SIGTERM, &a, 0);
+	sigaction (SIGINT, &a, 0);
+
+	return 0;
+#else //__WIN32__
+	signal (SIGINT, func);
+	signal (SIGTERM, func);
+	return 0;
+#endif
+}
+
---
+++ include/sighandler.h
@@ -0,0 +1,8 @@
+
+#ifndef _CVPN_SIGHANDLER_H
+#define _CVPN_SIGHANDLER_H
+
+int setup_sighandler(void(*)(int));
+
+#endif
+
