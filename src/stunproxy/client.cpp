6f28d4b2b3fe9764884161a9b1be7b51ccdade80
Mirek Kratochvil <exa.exa@gmail.com>
2009-04-29 22:39:06 +0200

    restructuralization
    cloudvpn renamed to cloud
    stunproxy moved from python to C++
    both moved to other programs
    'clients/' is now 'src/' with subdirs

---
+++ src/stunproxy/client.cpp
@@ -0,0 +1,83 @@
+
+/*
+ * Stun Proxy, also known as stunconn. Part of CloudVPN.
+ *
+ * configuration:
+ *
+ * listen, forward: local addresses to connect
+ * key: shared key to use for stunconn
+ * stunconn: some address to stunconn server.
+ */
+
+#include "sq.h"
+#include "log.h"
+#include "conf.h"
+#include "network.h"
+#include "timestamp.h"
+
+#include <stdint.h>
+
+int udp_fd,proxy_fd;
+
+class conn {
+public:
+	int fd;
+	int id;
+
+	//sending part
+	uint32_t next_send_packet;
+	uint64_t last_send_time;
+	map<uint32_t,pbuffer> waiting_acks;
+
+	//receiving part
+	uint32_t next_recv_packet;
+	uint64_t last_recv_time;
+	map<uint32_t,puffer>recvd;
+};
+
+map<int,conn>conns;
+
+
+int handle_udp()
+{
+
+}
+
+int handle_connect()
+{
+
+}
+
+int handle_accept()
+{
+
+}
+
+int stun_request()
+{
+
+}
+
+int http_request()
+{
+
+}
+
+int wait_and_poll()
+{
+
+}
+
+int init()
+{
+
+}
+
+int shutdown()
+{
+
+}
+
+int main() {
+	return 0;
+}
